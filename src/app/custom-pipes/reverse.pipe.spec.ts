d30d89815f638344a69c165f80a04efcab8baa64
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-20 22:05:16 +0200

    Test reverse pipe.

--- src/app/custom-pipes/reverse.pipe.spec.ts
+++ src/app/custom-pipes/reverse.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { ReversePipe } from './reverse.pipe';
 
 describe('ReversePipe', () => {
+
   it('create an instance', () => {
     const pipe = new ReversePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('reverses an array', () => {
+    const pipe = new ReversePipe();
+    const reversed = pipe.transform([1, 2, 3, 4, 5]);
+    expect(reversed).toEqual([5, 4, 3, 2, 1]);
   });
 });
