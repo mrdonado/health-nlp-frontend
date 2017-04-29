e21c46466f86e945ea958aa797e0e3e74701a37a
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-29 20:25:22 +0200

    BehaviorSubject as buffer instead of FirebaseListObservable.

--- src/app/main-components/demo/demo.component.spec.ts
+++ src/app/main-components/demo/demo.component.spec.ts
@@ -11,11 +11,28 @@ import { DemoComponent } from './demo.component';
 describe('DemoComponent', () => {
 
   let _collection = 'unknown';
+  let _options = {};
+  let subscriptions = 0;
 
   const AngularFireMock = {
     database: {
       list: (collection, options) => {
         _collection = collection;
+        _options = options;
+        return {
+          subscribe: (cb) => {
+            subscriptions += 1;
+            const demoAnalysis = {
+              message: 'Some message with a problem and a solution',
+              analysis: {
+                problem: 'a problem',
+                solution: 'a solution'
+              }
+            };
+            cb([Object.assign({ $key: 15 }, demoAnalysis),
+            Object.assign({ $key: 16 }, demoAnalysis)]);
+          }
+        }
       }
     }
   };
@@ -48,12 +65,14 @@ describe('DemoComponent', () => {
 
   it('should retrieve a collection /analysis', () => {
     expect(_collection).toEqual('/analysis');
+    expect(_options).toEqual({ query: { limitToLast: 5, orderByKey: true } });
   });
 
-  it('should increase the page size after running moreResults', () => {
-    expect(component.pageSize.getValue()).toEqual(5);
+  it('should request more items after running moreResults', () => {
+    subscriptions = 0;
     component.moreResults();
-    expect(component.pageSize.getValue()).toEqual(10);
+    expect(_options).toEqual({ query: { limitToLast: 6, orderByKey: true, endAt: 15 } });
+    expect(subscriptions).toEqual(1);
   });
 
   it('should colorize the message accordingly', () => {
@@ -65,7 +84,8 @@ describe('DemoComponent', () => {
       }
     };
     let htmlMessage = component.formatMessage(item);
-    expect(htmlMessage).toEqual('Some message with <strong class="problem">a problem</strong> and <strong class="solution">a solution</strong>');
+    expect(htmlMessage).toEqual('Some message with <strong class="problem">' +
+      'a problem</strong> and <strong class="solution">a solution</strong>');
 
     item = {
       message: 'Some message with a problem and no solution',
@@ -76,8 +96,6 @@ describe('DemoComponent', () => {
     };
     htmlMessage = component.formatMessage(item);
     expect(htmlMessage).toEqual('Some message with <strong class="problem">a problem</strong> and no solution');
-
-
   });
 
 });
--- src/app/main-components/demo/demo.component.ts
+++ src/app/main-components/demo/demo.component.ts
@@ -1,8 +1,7 @@
-import { Subject } from 'rxjs/Rx';
+import { Observable, Subject, BehaviorSubject } from 'rxjs/Rx';
 import { QueryList } from '@angular/core/core';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-demo',
@@ -12,53 +11,88 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class DemoComponent implements OnInit {
 
-  analysis: FirebaseListObservable<any[]>;
-  pageSize: BehaviorSubject<any>;
-  pageStep: number;
+  private analysis: BehaviorSubject<any[]>; // A list with the retrieved analysis from firebase
+  private pageStep: number; // The number of items requested each time
+  private lastKey: any; // The key of the last item, in order to allow pagination
 
-  constructor(af: AngularFire) {
+  constructor(private af: AngularFire) {
     this.pageStep = 5;
-    this.pageSize = new BehaviorSubject(this.pageStep);
+    this.analysis = new BehaviorSubject([]);
+  }
 
-    this.analysis = af.database.list('/analysis',
+  /**
+   * Add the first page of results while initializing.
+   */
+  ngOnInit() {
+    /**
+     * Instead of binding this list directly to the DOM, we will add every new item to
+     * the analysis BehaviorSubject, which will act as buffer.
+     *
+     * This way, the existing items won't be refreshed on the list whenever more items
+     * are requested, thus updating the DOM only for the new items.
+     */
+    this.af.database.list('/analysis',
       {
         query: {
-          limitToLast: this.pageSize,
+          limitToLast: this.pageStep,
           orderByKey: true
         }
+      }).subscribe((data) => {
+        this.analysis.next(data);
+        this.lastKey = data[0].$key;
       });
-
   }
 
-  ngOnInit() {
-
+  /**
+   * Whenever more results are requested, a new query will be performed, and the new
+   * items will be attached at the end of the list, enabling pagination.
+   *
+   * We do it this way instead of updating limitToLast with a BehaviorSubject because
+   * we don't want the DOM to be altered for the existing elements.
+   */
+  moreResults() {
+    this.af.database.list('/analysis',
+      {
+        query: {
+          endAt: this.lastKey,
+          limitToLast: this.pageStep + 1, // The extra item will match the last item of the current list
+          orderByKey: true
+        }
+      }).subscribe(values => {
+        this.analysis.next(values
+          .slice(0, values.length - 1) // That extra item shouldn't be displayed
+          .concat(this.analysis.getValue()));
+        this.lastKey = values[0].$key;
+      });
   }
+
+
 
   /**
    * It receives an item where a message is contained along with an analysis. From the analysis,
    * it identifies solution and problem (when available), and it returns a markup message where
-   * problem and solution receive css classes. 
-   * 
-   * E.g.: 
-   * 
+   * problem and solution receive css classes.
+   *
+   * E.g.:
+   *
    * 'Some message with a problem and a solution'
-   * 
+   *
    * produces:
-   * 
+   *
    * 'Some message with <strong class="problem">a problem</strong> and <strong class="solution">a solution</strong>
-   * 
+   *
    * @param item An analyzed job item
    * @returns message string: the markup version of the problem.
    */
   formatMessage(item: { message: string, analysis: { problem: string, solution: string } }) {
-    let problemIndex = item.message.indexOf(item.analysis.problem),
-      message = '';
+    const problemIndex = item.message.indexOf(item.analysis.problem);
+    let message = '';
     if (problemIndex > -1) {
       message = item.message.substring(0, problemIndex) +
         '<strong class="problem">' + item.analysis.problem +
         '</strong>' + item.message.substring(problemIndex + item.analysis.problem.length);
     }
-    let solutionIndex = message.indexOf(item.analysis.solution);
+    const solutionIndex = message.indexOf(item.analysis.solution);
     if (solutionIndex > -1) {
       message = message.substring(0, solutionIndex) +
         '<strong class="solution">' + item.analysis.solution +
@@ -66,9 +100,4 @@ export class DemoComponent implements OnInit {
     }
     return message;
   }
-
-  moreResults() {
-    this.pageSize.next(this.pageSize.getValue() + this.pageStep);
-  }
-
 }
