4b03408d927926bbc89f92c9eff5255033ab6f57
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-29 20:24:06 +0200

    Lint suggestions applied.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('health-nlp-frontend App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    //expect(page.getParagraphText()).toEqual('app works!');
+    // expect(page.getParagraphText()).toEqual('app works!');
   });
 });
