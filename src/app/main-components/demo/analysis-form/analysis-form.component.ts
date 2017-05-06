529426c1fe2e5f29a1ba2d437e23c7406cc95f38
F. Javier R. Donado <fjrd84@gmail.com>
2017-05-06 20:53:51 +0200

    Bind output to an EventEmitter.

--- src/app/main-components/demo/analysis-form/analysis-form.component.ts
+++ src/app/main-components/demo/analysis-form/analysis-form.component.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../../../environments/environment';
 import { Http } from '@angular/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-analysis-form',
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysisFormComponent implements OnInit {
 
+  @Output() closeCb = new EventEmitter();
+
   private newJobUrl: string;
   private userName: string;
   private userDescription: string;
@@ -16,12 +18,21 @@ export class AnalysisFormComponent implements OnInit {
 
   constructor(private http: Http) {
     this.newJobUrl = environment.analyzerUrl + '/analysis';
+  }
+
+  ngOnInit() {
+    this.clearForm();
+  }
+
+  clearForm() {
     this.userName = '';
     this.userDescription = '';
     this.message = '';
   }
 
-  ngOnInit() {
+  closeForm() {
+    this.clearForm();
+    this.closeCb.emit('close');
   }
 
   sendNewJob() {
@@ -31,7 +42,9 @@ export class AnalysisFormComponent implements OnInit {
       user_description: this.userDescription,
       message: this.message
     }).subscribe((response) => {
-      console.log(response.json())
+      console.log(response.json());
+      this.closeForm();
+      this.clearForm();
     });
   }
 
