ed1299cb48ad005b565877a0b1068eb9a5e64873
F. Javier R. Donado <fjrd84@gmail.com>
2017-05-06 20:00:45 +0200

    Basic input elements.

--- src/app/main-components/demo/analysis-form/analysis-form.component.spec.ts
+++ src/app/main-components/demo/analysis-form/analysis-form.component.spec.ts
@@ -1,16 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { AnalysisFormComponent } from './analysis-form.component';
+
+import {
+  BaseRequestOptions,
+  Http,
+  Response,
+  ResponseOptions,
+  RequestMethod,
+  XHRBackend
+} from '@angular/http';
+
+import {
+  MockBackend,
+  MockConnection
+} from '@angular/http/testing';
 
 describe('AnalysisFormComponent', () => {
   let component: AnalysisFormComponent;
   let fixture: ComponentFixture<AnalysisFormComponent>;
+  let backend: MockBackend;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnalysisFormComponent ]
+      declarations: [AnalysisFormComponent],
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -18,6 +43,11 @@ describe('AnalysisFormComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  beforeEach(inject([MockBackend],
+    (_sessionService, _mockBackend) => {
+      backend = _mockBackend;
+    }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/main-components/demo/analysis-form/analysis-form.component.ts
+++ src/app/main-components/demo/analysis-form/analysis-form.component.ts
@@ -1,3 +1,5 @@
+import { environment } from '../../../../environments/environment';
+import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysisFormComponent implements OnInit {
 
-  constructor() { }
+  private newJobUrl: string;
+  private userName: string;
+  private userDescription: string;
+  private message: string;
+
+  constructor(private http: Http) {
+    this.newJobUrl = environment.analyzerUrl + '/analysis';
+    this.userName = '';
+    this.userDescription = '';
+    this.message = '';
+  }
 
   ngOnInit() {
+  }
+
+  sendNewJob() {
+    this.http.post(this.newJobUrl, {
+      source: 'web',
+      user_name: this.userName,
+      user_description: this.userDescription,
+      message: this.message
+    }).subscribe((response) => {
+      console.log(response.json())
+    });
   }
 
 }
