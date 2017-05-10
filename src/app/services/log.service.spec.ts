ae04838333e1d00447d6ca137b4753ed2a060fcf
F. Javier R. Donado <fjrd84@gmail.com>
2017-05-10 22:05:31 +0200

    LogService as a message bus.

--- src/app/services/log.service.spec.ts
+++ src/app/services/log.service.spec.ts
@@ -3,13 +3,26 @@ import { TestBed, inject } from '@angular/core/testing';
 import { LogService } from './log.service';
 
 describe('LogService', () => {
+
+  let logService: LogService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [LogService]
     });
   });
 
-  it('should ...', inject([LogService], (service: LogService) => {
-    expect(service).toBeTruthy();
+  beforeEach(inject([LogService], (service: LogService) => {
+    logService = service;
   }));
+
+  it('should receive and broadcast log messages', (done) => {
+    logService.getMessageBus()
+      .subscribe((message) => {
+        expect(message).toEqual('Some random message');
+        done();
+      });
+    logService.sendMessage('Some random message');
+  });
 });
+
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,8 +1,28 @@
+import { Observable, Subject } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LogService {
 
-  constructor() { }
+  private messageBus: Subject<string>;
+
+  constructor() {
+    this.messageBus = new Subject<string>();
+  }
+
+  /**
+   * Subscribe to this message bus in order to get
+   * the log messages.
+   */
+  public getMessageBus(): Observable<string> {
+    return this.messageBus.asObservable();
+  }
+
+  /**
+   * Send a message to all the subscribers.
+   */
+  public sendMessage(message: string): void {
+    this.messageBus.next(message);
+  }
 
 }
